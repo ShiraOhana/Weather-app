4374a7470c22902bf2b69c8c97b3a1acaa20a3f3
ShiraOhana <Soshiraohana@gmail.com>
2022-01-16 14:53:40 +0200

    Showing current temperature

---
+++ src/script.js
@@ -0,0 +1,21 @@
+function displayTemperature(response) {
+  console.log(response);
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "45be73f84a5b597b7603c48501d80624";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=ashdod&appid=${apiKey}
+&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
